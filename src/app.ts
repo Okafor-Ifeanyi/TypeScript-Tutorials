68a1975ba97e4e4740f014fea347ae32d7c4917f
Okafor Ifeanyi <zeusifeanyi058@gmail.com>
2023-10-27 13:24:58 +0100

    Feat: DOM & Type Casting TS

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
+
+// inputs
+const type = document.querySelector("#type") as HTMLSelectElement
+const tofrom = document.querySelector("#tofrom") as HTMLSelectElement
+const details = document.querySelector("#details") as HTMLSelectElement
+const amount = document.querySelector("#amount") as HTMLSelectElement
+
+form.addEventListener('submit', (e: Event) => {
+    e.preventDefault();
+
+    console.log(
+        type.value,
+        tofrom.value,
+        details.value,
+        amount.value
+    )
+})
